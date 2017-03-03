da293878e590fab82d5aaa3821df1d2a66e9118d
Audrey Fang <audrey.fang@mail.utoronto.ca>
2017-03-03 12:52:21 -0500

    Minor UI fixes in restrictions.html

--- webApp/restrictions.js
+++ webApp/restrictions.js
@@ -1,11 +1,11 @@
 "use strict";
 
 $(document).ready(function(){
-  $("#submit1").click(function(){
+  $("#submit").click(function(){
     location.href='/result'
   });
   
-  $("#submit2").click(function(){
+  $("#skip").click(function(){
     location.href='/result'
   });
 })
