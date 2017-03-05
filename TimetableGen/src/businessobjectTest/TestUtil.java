f131e0b13bd33975bd2f2d2d4af8c3611a539962
U-DESKTOP-81OGQPG\adamf <Adam.fong2011@gmail.com>
2017-03-05 18:49:44 -0500

    Added JUnit 4 path

--- TimetableGen/src/businessobjectTest/TestUtil.java
+++ TimetableGen/src/businessobjectTest/TestUtil.java
@@ -1,7 +1,9 @@
 package businessobjectTest;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import businessobject.ClassTime;
 import businessobject.Course;
@@ -9,15 +11,34 @@ import businessobject.ClassTime;
 import businessobject.CourseOffering;
 import businessobject.TimeSlot;
 
+import enums.CampusType;
+import enums.ClassType;
+import enums.Day;
+import enums.SemesterType;
+
 public class TestUtil {
-	private static Course createCourse(){
-		//random course
+	
+	public static Course createCourse(){
+		//first course in json
 		String code = "ABP100Y1Y";
 		String name = "Intro to Academic Studies";
 		String description = "";
-		List <Integer> breadths = new LinkedList ();
-		int year = 2016;
+		List <Integer> breadths = new LinkedList<Integer> ();
+		List <TimeSlot> timeSlots = new LinkedList<TimeSlot> ();
+		List <ClassTime> cTimes = new LinkedList<ClassTime> ();
+		timeSlots.add(new TimeSlot(Day.TUESDAY,36000,10800));
+		cTimes.add(new ClassTime("L0101", timeSlots));
 		
-		return new Course();
+		int year = 2016;
+		String prerequisite="";
+		String exclusions = "";
+		Map <ClassType, List<ClassTime>> classTimes= new HashMap<ClassType, List<ClassTime>>();
+		classTimes.put(ClassType.LEC,cTimes);
+		
+		CampusType campus = CampusType.UTSG;
+		SemesterType sem = SemesterType.YEAR;
+		return new Course(name,code,description,prerequisite,exclusions,year,campus,sem,classTimes,breadths);
 	}
+	
+	
 }
