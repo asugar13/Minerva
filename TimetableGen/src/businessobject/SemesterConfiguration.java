7bd1418275894adf5c19e538b7fab1c2e3204f1f
justchew <songjoocho@gmail.com>
2017-03-06 00:58:41 -0500

    Created TimetableConfigurationGenerator interface
    Created associated classes required for TimetableConfigurationGenerator
    interface and laid groundwork for implementation

---
+++ TimetableGen/src/businessobject/SemesterConfiguration.java
@@ -0,0 +1,26 @@
+package businessobject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class SemesterConfiguration implements TimetableConfiguration{
+	
+	/** Maximum courses for a semester is 6 **/
+	private Set<CourseOffering> semester1; //set of courses in the 1st semester
+	private Set<CourseOffering> semester2; //set of courses in the 2nd semester
+	//private List<Timetable> possibleTimetables (don't worry about this yet, timetable generator will use this)
+	
+	public SemesterConfiguration(Set<CourseOffering> semester1, Set<CourseOffering> semester2){
+		this.semester1 = semester1;
+		this.semester2 = semester2;
+	}
+
+	@Override
+	public List<Set<CourseOffering>> getSemesterConfigurations() {
+		List<Set<CourseOffering>> semesterConfigurations = new ArrayList<>();
+		semesterConfigurations.add(semester1);
+		semesterConfigurations.add(semester2);
+		return semesterConfigurations;
+	}
+}
