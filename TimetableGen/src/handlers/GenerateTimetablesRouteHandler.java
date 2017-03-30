2b3843ee8b7412eeb2ffcd237f65b17bab2fa617
Kang Dae Woo <kangdae3@teach.cs.toronto.edu>
2017-03-30 14:46:46 -0400

    Finished implementing and testing generateCourseSelections method in GenerateTimetablesRouteHandler class

--- TimetableGen/src/handlers/GenerateTimetablesRouteHandler.java
+++ TimetableGen/src/handlers/GenerateTimetablesRouteHandler.java
@@ -6,11 +6,12 @@ import java.util.Set;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import businessobject.CourseSelection;
 import businessobject.SemesterConfiguration;
-import businessobject.TimetableConfiguration;
 import dao.CourseListingDao;
+import enums.SemesterType;
 import generation.SemesterConfigurationGenerator;
 import generation.TimetableConfigurationGenerator;
 import generation.TimetableGenerator;
@@ -51,9 +52,48 @@ public class GenerateTimetablesRouteHandler implements Route {
 		return resJson.toJSONString();
 	}
 	
-	private Set<CourseSelection> generateCourseSelections(JSONObject reqJson){
-		//TODO
-		return null;
+	public Set<CourseSelection> generateCourseSelections(JSONObject reqJson){
+		Set<CourseSelection> result = new HashSet<>();
+		
+		JSONArray courses = (JSONArray) reqJson.get("courses");
+		for (int i1 = 0; i1 < courses.size(); i1++) {
+			String courseCode = (String) ((JSONObject) courses.get(i1)).get("courseCode");
+			JSONArray semesters = (JSONArray) ((JSONObject) courses.get(i1)).get("semesters");
+			
+			Set<SemesterType> semesterRestrictions = new HashSet<>();
+			for (int i2 = 0; i2 < semesters.size(); i2++) {
+				switch ((String) semesters.get(i2)) {
+				case "F":
+					semesterRestrictions.add(SemesterType.FALL);
+					break;
+				case "S":
+					semesterRestrictions.add(SemesterType.WINTER);
+					break;
+				case "Y":
+					semesterRestrictions.add(SemesterType.FALL);
+					semesterRestrictions.add(SemesterType.WINTER);
+					break;
+				default:
+					break;
+				}
+			}
+			
+			result.add(new CourseSelection(courseCode, semesterRestrictions));
+			// for testing purposes:
+			System.out.println((new CourseSelection(courseCode, semesterRestrictions).toString()));
+		}
+		return result;
 	}
 
+	public static void main(String[] args) throws ParseException {
+		JSONParser p = new JSONParser();
+		JSONObject o = (JSONObject) p.parse("{\"courses\": ["
+				+ "{\"courseCode\": \"CSC301H1\", \"semesters\": [\"F\"]},"
+				+ "{\"courseCode\": \"CSC302H1\", \"semesters\": [\"S\"]},"
+				+ "{\"courseCode\": \"CSC373H1\", \"semesters\": [\"F\", \"S\"]},"
+				+ "{\"courseCode\": \"PSY100Y1\", \"semesters\": [\"Y\"]}"
+				+ "]}");
+		GenerateTimetablesRouteHandler g = new GenerateTimetablesRouteHandler(null);
+		g.generateCourseSelections(o);
+	}
 }
