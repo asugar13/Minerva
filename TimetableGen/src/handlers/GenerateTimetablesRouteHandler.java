3748060b5abb18e39c7ab79fe7b3c66653edaa69
justchew <songjoocho@gmail.com>
2017-04-02 22:56:21 -0400

    Wired up sorting and hashing logic

--- TimetableGen/src/handlers/GenerateTimetablesRouteHandler.java
+++ TimetableGen/src/handlers/GenerateTimetablesRouteHandler.java
@@ -1,6 +1,10 @@
 package handlers;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.json.simple.JSONArray;
@@ -12,6 +16,7 @@ import businessobject.CourseSelection;
 import businessobject.SemesterConfiguration;
 import dao.CourseListingDao;
 import enums.SemesterType;
+import enums.TimetableComparators;
 import generation.SemesterConfigurationGenerator;
 import generation.TimetableConfigurationGenerator;
 import generation.TimetableGenerator;
@@ -21,8 +26,12 @@ import spark.Route;
 
 public class GenerateTimetablesRouteHandler implements Route {
 	CourseListingDao listingDao;
-	public GenerateTimetablesRouteHandler(CourseListingDao listingDao){
+	SortingHandler sorter;
+	Map<Integer, Set<SemesterConfiguration>> cache = new HashMap<>();
+
+	public GenerateTimetablesRouteHandler(CourseListingDao listingDao, SortingHandler sorter) {
 		this.listingDao = listingDao;
+		this.sorter = sorter;
 	}
 
 	@SuppressWarnings("unchecked")
@@ -33,33 +42,71 @@ public class GenerateTimetablesRouteHandler implements Route {
 		JSONObject reqJson = (JSONObject) parser.parse(reqJsonString);
 		Set<CourseSelection> courses = generateCourseSelections(reqJson);
 		TimetableConfigurationGenerator configGen = new SemesterConfigurationGenerator(listingDao);
-		Set<SemesterConfiguration> configurations = configGen.generateConfigurations(courses);
-		TimetableGenerator generator = new TimetableGenerator();
-		Set<SemesterConfiguration> possibleTimetableConfigurations = new HashSet<>();
-		for(SemesterConfiguration configuration: configurations){
-			possibleTimetableConfigurations.add(generator.generate(configuration));
+
+		// Get configurations from cache if null then generate
+		// TODO ensure hash works
+		Set<SemesterConfiguration> configurations = cache.get(courses.hashCode()); 
+		Set<SemesterConfiguration> possibleTimetableConfigurations = null;
+		if (configurations == null) {
+			configurations = configGen.generateConfigurations(courses);
+			TimetableGenerator generator = new TimetableGenerator();
+			possibleTimetableConfigurations = new HashSet<>();
+			for (SemesterConfiguration configuration : configurations) {
+				possibleTimetableConfigurations.add(generator.generate(configuration));
+			}
+			
+			//Put generated timetables into the cache
+			cache.put(courses.hashCode(), configurations);
+		} else{
+			
+			//Copy cached set
+			possibleTimetableConfigurations = new HashSet<>(configurations);
 		}
+		
+
+		// Sort if sorting is specified
+		List<TimetableComparators> comparators = createTimetableComparators(reqJson);
+		Set<SemesterConfiguration> sortedConfigurations = null;
+		if (comparators.size() > 0) {
+			sortedConfigurations = new HashSet<>();
+			for (SemesterConfiguration configuration : possibleTimetableConfigurations) {
+				sortedConfigurations.add(sorter.TimetableSort(new ArrayList<>(), comparators, configuration));
+			}
+		} else {
+			sortedConfigurations = possibleTimetableConfigurations;
+		}
+
+		// Convert to JSON
 		JSONObject resJson = new JSONObject();
 		JSONArray configurationsJson = new JSONArray();
 		int i = 0;
-		for(SemesterConfiguration possibleConfig : possibleTimetableConfigurations){
+		for (SemesterConfiguration possibleConfig : sortedConfigurations) {
 			JSONObject possibleConfigJson = possibleConfig.toJsonObject();
 			possibleConfigJson.put("configurationNumber", i);
 			configurationsJson.add(possibleConfigJson);
-			i ++;
+			i++;
 		}
 		resJson.put("configurations", configurationsJson);
 		return resJson.toJSONString();
 	}
-	
-	public Set<CourseSelection> generateCourseSelections(JSONObject reqJson){
+
+	private List<TimetableComparators> createTimetableComparators(JSONObject reqJson) {
+		JSONArray filters = (JSONArray) reqJson.get("filters");
+		List<TimetableComparators> comparators = new ArrayList<>();
+		for (int i = 0; i < filters.size(); i++) {
+			comparators.add(TimetableComparators.valueOf(filters.get(i).toString()));
+		}
+		return comparators;
+	}
+
+	private Set<CourseSelection> generateCourseSelections(JSONObject reqJson) {
 		Set<CourseSelection> result = new HashSet<>();
-		
+
 		JSONArray courses = (JSONArray) reqJson.get("courses");
 		for (int i1 = 0; i1 < courses.size(); i1++) {
 			String courseCode = (String) ((JSONObject) courses.get(i1)).get("courseCode");
 			JSONArray semesters = (JSONArray) ((JSONObject) courses.get(i1)).get("semesters");
-			
+
 			Set<SemesterType> semesterRestrictions = new HashSet<>();
 			for (int i2 = 0; i2 < semesters.size(); i2++) {
 				switch ((String) semesters.get(i2)) {
@@ -77,7 +124,7 @@ public class GenerateTimetablesRouteHandler implements Route {
 					break;
 				}
 			}
-			
+
 			result.add(new CourseSelection(courseCode, semesterRestrictions));
 			// for testing purposes:
 			System.out.println((new CourseSelection(courseCode, semesterRestrictions).toString()));
@@ -87,13 +134,12 @@ public class GenerateTimetablesRouteHandler implements Route {
 
 	public static void main(String[] args) throws ParseException {
 		JSONParser p = new JSONParser();
-		JSONObject o = (JSONObject) p.parse("{\"courses\": ["
-				+ "{\"courseCode\": \"CSC301H1\", \"semesters\": [\"F\"]},"
-				+ "{\"courseCode\": \"CSC302H1\", \"semesters\": [\"S\"]},"
-				+ "{\"courseCode\": \"CSC373H1\", \"semesters\": [\"F\", \"S\"]},"
-				+ "{\"courseCode\": \"PSY100Y1\", \"semesters\": [\"Y\"]}"
-				+ "]}");
-		GenerateTimetablesRouteHandler g = new GenerateTimetablesRouteHandler(null);
+		JSONObject o = (JSONObject) p
+				.parse("{\"courses\": [" + "{\"courseCode\": \"CSC301H1\", \"semesters\": [\"F\"]},"
+						+ "{\"courseCode\": \"CSC302H1\", \"semesters\": [\"S\"]},"
+						+ "{\"courseCode\": \"CSC373H1\", \"semesters\": [\"F\", \"S\"]},"
+						+ "{\"courseCode\": \"PSY100Y1\", \"semesters\": [\"Y\"]}" + "]}");
+		GenerateTimetablesRouteHandler g = new GenerateTimetablesRouteHandler(null, null);
 		g.generateCourseSelections(o);
 	}
 }
--- TimetableGen/src/http/RestServer.java
+++ TimetableGen/src/http/RestServer.java
@@ -8,24 +8,27 @@ import org.json.simple.parser.JSONParser;
 import dao.CourseListingDao;
 import dao.CourseLoader;
 import handlers.CourseInformationRouteHandler;
+import handlers.GenerateTimetablesRouteHandler;
+import handlers.SortingHandler;
 import handlers.TestRoute;
 
 public class RestServer {
 	
 	private static String FILE_PATH = "res/courses.json";
 	private static CourseListingDao listingDao;
+	private static SortingHandler sorter;
 	
 	public static void main(String[] args) {
 		createDependencies();
 		port(8800);
-		JSONParser parser = new JSONParser();
 		
-		JSONObject obj = new JSONObject();
-		obj.put("get", "info");
+		//Routes
 		get("/course-information",  new CourseInformationRouteHandler(listingDao));
 		get("/test",  new TestRoute());
+		get("/generate-timetable", new GenerateTimetablesRouteHandler(listingDao, sorter));
+		System.out.println("Server started");
 		
-
+		/*
 		post("/generate-timetable", (req, res) -> {
 			res.header("Access-Control-Allow-Origin", "*");
 			
@@ -35,12 +38,12 @@ public class RestServer {
 			// TODO: Generate and return timetable based on body
 			return obj.toJSONString();
 		});
-
-		System.out.println("Server started");
+		*/
 	}
 	
 	private static void createDependencies(){
 		listingDao = new CourseLoader(FILE_PATH);
+		sorter = new SortingHandler();
 	}
 
 }
